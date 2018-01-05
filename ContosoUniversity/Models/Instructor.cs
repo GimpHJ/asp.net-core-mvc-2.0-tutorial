e17bc0218e5dcfc74638a1726fcaaf786325bd07
hyungju.lim <hyungju.lim@ebankcardservice.com>
2018-01-05 13:33:40 -0800

    https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/update-related-data

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -33,7 +33,19 @@ namespace ContosoUniversity.Models
             get { return LastName + ", " + FirstMidName; }
         }
 
-        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        //public ICollection<CourseAssignment> CourseAssignments { get; set; }
+
+        private ICollection<CourseAssignment> _courseAssignments;
+
+        public ICollection<CourseAssignment> CourseAssignments
+        {
+            get
+            {
+                return _courseAssignments ?? (_courseAssignments = new List<CourseAssignment>());
+            }
+            set { _courseAssignments = value; }
+        }
+
         public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
